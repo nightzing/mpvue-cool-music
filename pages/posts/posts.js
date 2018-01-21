b4fb303b35c27412e8a8487ea40f5d3f709f0ab5
x751685875 <751685875@qq.com>
2018-01-21 10:43:46 +0800

    first

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -2,7 +2,7 @@ const postsData = require('../../data/posts-data.js')
 Page({
     /*** 页面的初始数据*/
     data: {
-        post_key : postsData.post_content
+        post_key : postsData.post_content 
     },
     /*** 生命周期函数--监听页面加载*/
     onLoad: function () {
